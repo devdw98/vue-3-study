b5ff1c0399314eefe3f09b21ddeaf3a4269554f4
dowon <devdw98@gmail.com>
2020-12-24 14:08:29 +0900

    L5 - for

--- begin/main.js
+++ begin/main.js
@@ -8,7 +8,14 @@ const app = Vue.createApp({
             myUrl:"https://kr.ncsoft.com/kr/index.do",
             inventory:0,
             inStock2: true,
-            onSale: true
+            onSale: true,
+
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color:'green'},
+                {id: 2235, color:'blue'},
+            ],
+            sizes: ['S', 'M', 'L', 'XL'],
         }
     }
 })
