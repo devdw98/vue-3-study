f4d383ba27c60bf136f8fc4aa748aa57af5e6c57
dowon <devdw98@gmail.com>
2020-12-24 13:35:42 +0900

    L3 - attribute binding (v-bind)

--- begin/main.js
+++ begin/main.js
@@ -3,6 +3,9 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             description: 'this is description.',
+            image: './assets/images/socks_green.jpg',
+            image2:'./assets/images/socks_blue.jpg',
+            myUrl:"https://kr.ncsoft.com/kr/index.do",
         }
     }
 })
