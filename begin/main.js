598aa88ec303a1d9417503151f8c5f5ce1431f84
dowon <devdw98@gmail.com>
2020-12-24 13:51:20 +0900

    L4 - conditional

--- begin/main.js
+++ begin/main.js
@@ -6,6 +6,9 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             image2:'./assets/images/socks_blue.jpg',
             myUrl:"https://kr.ncsoft.com/kr/index.do",
+            inventory:0,
+            inStock2: true,
+            onSale: true
         }
     }
 })
