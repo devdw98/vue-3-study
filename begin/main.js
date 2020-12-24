debaa29973a369f2bb94cdeeadd0b57e47890546
dowon <devdw98@gmail.com>
2020-12-24 15:19:29 +0900

    clear and L9 start

--- begin/main.js
+++ begin/main.js
@@ -1,40 +1,45 @@
 const app = Vue.createApp({
-    data: function(){
+    data() {
         return {
+            cart:0,
             product: 'Socks',
-            description: 'this is description.',
-            image: './assets/images/socks_green.jpg',
-            image2:'./assets/images/socks_blue.jpg',
-            myUrl:"https://kr.ncsoft.com/kr/index.do",
-            inventory:0,
-            inStock2: false,
-            onSale: true,
-
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 2234, color:'green', image:'./assets/images/socks_green.jpg'},
-                {id: 2235, color:'blue', image:'./assets/images/socks_blue.jpg'},
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ],
-            sizes: ['S', 'M', 'L', 'XL'],
-            cart:0,
+            // solution
+            onSale: true
+            // solution
         }
     },
-    methods:{
-        addToCart(){
-            this.cart += 1;
-            console.log(this.cart);
+    methods: {
+        addToCart() {
+            this.cart += 1
         },
-        updateImage(variantImage){
-            this.image2 = variantImage;
-        },
-        decreaseToCart(){
-            if(this.cart > 0){
-                this.cart -= 1;
-            }else{
-                this.cart = 0;
-            }
-            
-            console.log(this.cart);
+        updateVariant(index) {
+            this.selectedVariant = index
         }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        // solution
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' is on sale.'
+            }
+            return ''
+        }
+        // solution
     }
 })
